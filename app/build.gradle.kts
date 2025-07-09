74a9ae480898dc039da77f2cab1743416d6bd970
mode:hate <s.deryabin175@gmail.com>
2025-07-09 10:52:27 +0300

    theme & font was added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,9 @@ android {
 dependencies {
     implementation(libs.apollo.runtime)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.navigation.compose)
 
+    implementation(libs.androidx.ui.text.google.fonts)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
